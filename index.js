903f0ba92dda25a183975e7285312f9cdc182f80
Mike Robards <mikerobards@icloud.com>
2021-03-24 15:58:08 -0400

    add email to index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 let emailCollectorForm = document.getElementById("Email-Collector")
-emailCollectorForm.addEventListener("submit", e=> {
+emailCollectorForm.addEventListener("submit", e => {
     e.preventDefault()
 
     let ourFormData = new FormData(e.target)
 
     let userFirstName = ourFormData.get("firstName")
+    let userEmailAddress = ourFormData.get("emailAddress")
 
-    let updatedHtmlContent = `    <h2>Congratulations, ${userFirstName}!</h2>
+    let updatedHtmlContent = `    
+    <h2>Congratulations, ${userFirstName}!</h2>
+    
     <p>You're on your way to becoming a BBQ Master!</p>
     
     <p class="fine-print">
-      We'll never share your information without your permission
-    </p>`
-
+    You will get weekly BBQ tips sent to: ${userEmailAddress}
+    </p>
+    `
+    
     let ourMainContent = document.getElementById("Main-Content")
     ourMainContent.innerHTML = updatedHtmlContent
 
